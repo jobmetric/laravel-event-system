bf18faa47a370538adadd792972311be0af688fa
majid mohammadian <majeedmohammadian@gmail.com>
2024-08-17 23:22:43 +0330

    add language file

---
+++ lang/en/base.php
@@ -0,0 +1,28 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Base Event System Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during Event System for
+    | various messages that we need to display to the user.
+    |
+    */
+
+    "validation" => [
+        "errors" => "Validation errors occurred."
+    ],
+
+    "messages" => [
+        "created" => "Event System created successfully.",
+        "deleted" => "Event System deleted successfully.",
+    ],
+
+    "exceptions" => [
+        "event_system_not_found" => "Event System with number :number not found."
+    ],
+
+];
