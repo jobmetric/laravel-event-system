0f8535b4c8df21d8a75a5a8154ed80e0baf97563
majid mohammadian <majeedmohammadian@gmail.com>
2024-08-17 23:22:52 +0330

    change service provider

--- src/EventSystemServiceProvider.php
+++ src/EventSystemServiceProvider.php
@@ -2,7 +2,9 @@
 
 namespace JobMetric\EventSystem;
 
+use JobMetric\PackageCore\Enums\RegisterClassTypeEnum;
 use JobMetric\PackageCore\Exceptions\MigrationFolderNotFoundException;
+use JobMetric\PackageCore\Exceptions\RegisterClassTypeNotFoundException;
 use JobMetric\PackageCore\PackageCore;
 use JobMetric\PackageCore\PackageCoreServiceProvider;
 
@@ -13,12 +15,14 @@ class EventSystemServiceProvider extends PackageCoreServiceProvider
      *
      * @return void
      * @throws MigrationFolderNotFoundException
+     * @throws RegisterClassTypeNotFoundException
      */
     public function configuration(PackageCore $package): void
     {
         $package->name('laravel-event-system')
             ->hasConfig()
             ->hasMigration()
-            ->hasTranslation();
+            ->hasTranslation()
+            ->registerClass('event', EventServiceProvider::class, RegisterClassTypeEnum::REGISTER());
     }
 }
