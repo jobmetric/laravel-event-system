218e419c5307e7f5bf4720b431f9aabe9aebe9ba
majid mohammadian <majeedmohammadian@gmail.com>
2024-08-17 23:24:36 +0330

    add facade

---
+++ src/Facades/EventSystem.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace JobMetric\EventSystem\Facades;
+
+use Illuminate\Support\Facades\Facade;
+
+/**
+ * @method static \Spatie\QueryBuilder\QueryBuilder query(array $filter = [])
+ * @method static \Illuminate\Http\Resources\Json\AnonymousResourceCollection paginate(array $filter = [], int $page_limit = 15)
+ * @method static \Illuminate\Http\Resources\Json\AnonymousResourceCollection all(array $filter = [])
+ * @method static array store(array $data)
+ * @method static array delete(int $event_system_id)
+ *
+ * @see \JobMetric\EventSystem\EventSystem
+ */
+class EventSystem extends Facade
+{
+    /**
+     * Get the registered name of the component.
+     *
+     * @return string
+     */
+    protected static function getFacadeAccessor(): string
+    {
+        return \JobMetric\EventSystem\EventSystem::class;
+    }
+}
