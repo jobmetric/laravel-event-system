68dbe8ecd1a3cc2f65e8015a136bd80bb3772ed7
majid mohammadian <majeedmohammadian@gmail.com>
2024-08-18 14:02:11 +0330

    add class exist rule

---
+++ src/Rules/ClassExistRule.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace JobMetric\EventSystem\Rules;
+
+use Closure;
+use Illuminate\Contracts\Validation\ValidationRule;
+use Illuminate\Translation\PotentiallyTranslatedString;
+
+class ClassExistRule implements ValidationRule
+{
+    /**
+     * Run the validation rule.
+     *
+     * @param Closure(string): PotentiallyTranslatedString $fail
+     */
+    public function validate(string $attribute, mixed $value, Closure $fail): void
+    {
+        if (!class_exists($value)) {
+            $fail(__('event-system::base.validation.class_exist', [
+                'class' => $value
+            ]));
+        }
+    }
+}
