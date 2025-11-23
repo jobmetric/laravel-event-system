54d84d59cb0e14ef7e791614f07b197ac13ce9d3
majidmohammadian <majeedmohammadian@gmail.com>
2025-11-23 20:23:32 +0330

    added some testcase for EventRegistry

--- tests/Feature/EventRegistryTest.php
+++ tests/Feature/EventRegistryTest.php
@@ -3,6 +3,7 @@
 namespace JobMetric\EventSystem\Tests\Feature;
 
 use InvalidArgumentException;
+use JobMetric\EventSystem\Support\DomainEventDefinition;
 use JobMetric\EventSystem\Support\EventRegistry;
 use JobMetric\EventSystem\Tests\Stubs;
 use JobMetric\EventSystem\Tests\TestCase as BaseTestCase;
@@ -23,12 +24,32 @@ class EventRegistryTest extends BaseTestCase
         $this->assertSame(Stubs\Events\DomainEventExample::key(), $registry->keyFor($instance));
     }
 
+    public function test_register_stores_definition_and_allows_lookup(): void
+    {
+        $registry = new EventRegistry();
+        $registry->register(Stubs\Events\DomainEventExample::key(), Stubs\Events\DomainEventExample::class);
+
+        $definition = $registry->definitionForKey(Stubs\Events\DomainEventExample::key());
+
+        $this->assertInstanceOf(DomainEventDefinition::class, $definition);
+        $this->assertSame(Stubs\Events\DomainEventExample::key(), $definition->key);
+        $this->assertSame('tests', $definition->group);
+        $this->assertSame('Domain Event Example', $definition->title);
+        $this->assertSame('Example domain event for tests', $definition->description);
+        $this->assertSame('fa fa-test', $definition->icon);
+        $this->assertSame(['tests', 'event'], $definition->tags);
+
+        $definitions = $registry->allDefinitions();
+        $this->assertArrayHasKey(Stubs\Events\DomainEventExample::key(), $definitions);
+        $this->assertSame($definition, $definitions[Stubs\Events\DomainEventExample::key()]);
+    }
+
     public function test_register_throws_for_invalid_class(): void
     {
         $registry = new EventRegistry();
 
         $this->expectException(InvalidArgumentException::class);
-        $this->expectExceptionMessage('[JobMetric\\EventSystem\\Tests\\Stubs\\Events\\PlainEvent] must implement DomainEvent interface.');
+        $this->expectExceptionMessage('[JobMetric\\EventSystem\\Tests\\Stubs\\Events\\PlainEvent] must implement JobMetric\\EventSystem\\Contracts\\DomainEvent.');
 
         $registry->register('invalid-event', Stubs\Events\PlainEvent::class);
     }
@@ -39,5 +60,6 @@ class EventRegistryTest extends BaseTestCase
 
         $this->assertNull($registry->forKey('missing.key'));
         $this->assertNull($registry->keyFor(Stubs\Events\PlainEvent::class));
+        $this->assertNull($registry->definitionForKey('missing.key'));
     }
 }
--- tests/Stubs/Events/DomainEventExample.php
+++ tests/Stubs/Events/DomainEventExample.php
@@ -3,6 +3,7 @@
 namespace JobMetric\EventSystem\Tests\Stubs\Events;
 
 use JobMetric\EventSystem\Contracts\DomainEvent;
+use JobMetric\EventSystem\Support\DomainEventDefinition;
 
 class DomainEventExample implements DomainEvent
 {
@@ -19,5 +20,17 @@ class DomainEventExample implements DomainEvent
     public static function key(): string
     {
         return 'tests.event.bus.domain';
+    }
+
+    public static function definition(): DomainEventDefinition
+    {
+        return new DomainEventDefinition(
+            key: self::key(),
+            group: 'tests',
+            title: 'Domain Event Example',
+            description: 'Example domain event for tests',
+            icon: 'fa fa-test',
+            tags: ['tests', 'event'],
+        );
     }
 }
